224bf0056c253c78f2eda24219c7d5a5cbe9809d
Luis Seidel <luis.guilherme.seidel@gmail.com>
2022-03-09 19:52:53 -0300

    init

--- header.php
+++ header.php
@@ -5,12 +5,90 @@
     <title><?php bloginfo('name'); ?> &raquo; <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-    <header class="my-logo">
-        <h1><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
+
+    <header>
+        <div class="menu-mobile container">
+            <div class="menu mobile">
+                <button class="menu-btn" onclick="toggle()">
+                    <i class="fa-solid fa-bars fa-2xl" aria-hidden="true"></i>
+                </button>
+            </div>
+
+            <div class="logo-container">
+                <?php
+                    $custom_logo_id = get_theme_mod( 'custom_logo' );
+                    $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+                ?>
+                <img class="logo" src="<?= $image[0]; ?>" alt="logo">
+            </div>
+
+            <div class="links-container">
+                <div class="menu-links desktop">
+                    <ul>
+                        <li>
+                            <a href="#quem-somos">
+                                <p>Quem Somos</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#servicos">
+                                <p>Serviços</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#pneus">
+                                <p>Pneus</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#contato">
+                                <p>Contato</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </header>
-    <?php wp_nav_menu(array('header-menu' => 'header-menu')); ?>
+
+    <div class="sidebar">
+        <div class="blocker"></div>
+        <div class="content">
+            <div class="sidebar-header">
+                <div class="close-btn">
+                    <button class="menu-btn" onclick="hide()">
+                        <i class="fa-solid fa-xmark fa-2xl"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="sidebar-main">
+                <ul>
+                    <li>
+                        <a href="#quem-somos" onclick="hide()">
+                            <p>Quem Somos</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#servicos" onclick="hide()">
+                            <p>Serviços</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#pneus" onclick="hide()">
+                            <p>Pneus</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contato" onclick="hide()">
+                            <p>Contato</p>
+                        </a>
+                    </li>
+                </ul>
+                
+            </div>
+        </div>
+    </div>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,100 @@
-print("version: 1.0.0");
+function show() {
+	const sidebar = document.querySelector(".sidebar");
+	sidebar.classList.add("visible");
+	document.body.style.overflow = "hidden";
+}
+function hide() {
+	const sidebar = document.querySelector(".sidebar");
+	sidebar.classList.remove("visible");
+	document.body.style.overflow = "";
+}
+function toggle() {
+	const sidebar = document.querySelector(".sidebar");
+	sidebar.querySelector(".blocker").onclick = hide;
+	sidebar.classList.contains("visible") ? hide() : show();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.querySelector('.contato-form');
+    if (form) {
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+
+            var phone = "5551981842396";
+            var nome = document.querySelector('#nome').value.trim();
+            var text = document.querySelector('#mensagem').value.trim();
+
+            if (nome == "" || text == "") {
+                window.alert("Preencha corretamente os campos!");
+                form.reset();
+                return;
+            }
+
+            var mensagem = `Nome: ${nome} Mensagem: ${text}`;
+            mensagem = encodeURIComponent(mensagem);
+            var link = `https://api.whatsapp.com/send?phone=${phone}&text=${mensagem}`;
+
+            window.open(link);
+            form.reset();
+        });
+    }
+
+    if (document.querySelector('.slider-banner.mobile')) {
+        var slider = tns({
+            container: '.slider-banner.mobile',
+            items: 1,
+            slideBy: 'page',
+            autoplay: true,
+            controls: false,
+            mouseDrag: true,
+            nav: false,
+            autoplayButton: false,
+            autoplayButtonOutput: false,
+          });
+    }
+
+    if (document.querySelector('.slider-banner.desktop')) {
+        var slider = tns({
+            container: '.slider-banner.desktop',
+            items: 1,
+            slideBy: 'page',
+            autoplay: true,
+            controls: false,
+            mouseDrag: true,
+            nav: false,
+            autoplayButton: false,
+            autoplayButtonOutput: false,
+          });
+    }
+
+    // if (document.querySelector('.slider-pneus')) {
+    //     var slider = tns({
+    //         container: '.slider-pneus',
+    //         items: 1,
+    //         slideBy: 1,
+    //         autoplay: true,
+    //         controls: false,
+    //         mouseDrag: true,
+    //         nav: true,
+    //         navPosition: "bottom",
+    //         autoplayButton: false,
+    //         autoplayButtonOutput: false,
+    //         responsive: {
+    //             600: {
+    //                 edgePadding: 20,
+    //                 gutter: 20,
+    //                 items: 2,
+    //                 slideBy: 2,
+    //             },
+    //             700: {
+    //                 gutter: 30
+    //             },
+    //             900: {
+    //                 items: 3,
+    //                 slideBy: 3
+    //             },
+    //         }
+    //     });
+    //}
+
+});
